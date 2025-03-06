7c936f63f41f88a2a047980ac0270812b712f53f
ojg <jeongkeepscalm@gmail.com>
2025-03-06 10:00:19 +0900

    .

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react";
 import QUESTIONS from "../questions.js";
-import quizCompletedImage from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -22,10 +22,7 @@ export default function Quiz() {
 
   if (quizIsComplete) {
     return (
-      <div id="summary">
-        <img src={quizCompletedImage} alt="quiz done"></img>
-        <h2>Quiz Completed!</h2>
-      </div>
+      <Summary userAnswers={userAnswers}/>
     );
   }
 
